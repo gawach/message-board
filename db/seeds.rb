4dbea5e8ab94ea01d7d68e09107d2a4b974e2d0f
gawach <gawa9.1998@gmail.com>
2020-06-12 06:39:41 +0000

    ReAdd pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |num|
+  Message.create(title: "test title #{num}", content: "test content #{num}")
+end
